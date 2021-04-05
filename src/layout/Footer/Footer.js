8a4d2f5325aba5932bc2a0ba44858bf7a94d639b
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-04-05 20:37:10 +0200

    Update 5.04.2021 20:36
    Edit social media in footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,25 +1,44 @@
 import React from "react";
+import "./style.css";
+import socials from "./social";
 
 export default function Footer() {
   return (
     <footer className="w-full bg-white dark:bg-gray-800 dark:text-white anime bottom-0 ">
-      <div className="flex justify-around">
-        <div>
-          <h1>About Us</h1>
-          <p>Lorem ipsum dolor sit amet.</p>
+      <div className="grid grid-cols-2 grid-rows-2 p-4 mt-4">
+        <div className="place">
+          <h1 className="pb-2">About Us</h1>
+          <p>Lorem </p>
         </div>
-        <div>
-          <h1>Services</h1>
+        <div className="place">
+          <h1 className="pb-2">Services</h1>
           <p>Lorem, ipsum dolor.</p>
         </div>
-        <div>
-          <h1>Contact</h1>
-          <p>Lorem ipsum dolor sit.</p>
+
+        <div className="place">
+          <h1 className="pb-2">Contact</h1>
+          <a>Napisz do nas!</a>
         </div>
-        <div>
-          <h1>Social</h1>
-          <p>Lorem, ipsum.</p>
-          {/* <i className="fas fa-twitter text-pink-800">cos</i> */}
+        <div className="place">
+          <h1 className="pb-2">Social</h1>
+          <div>
+            {socials.map((social, index) => {
+              return (
+                <a
+                  href={social.path}
+                  key={index}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i
+                    className={`fab fa-${social.icons} py-1 pr-2 grid grid-cols-4 text-center`}
+                  >
+                    <p className="grid col-auto">{social.title}</p>
+                  </i>
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
     </footer>
---
+++ src/layout/Footer/social.js
@@ -0,0 +1,23 @@
+const social = [
+    {
+        title: "Facebook",
+        path: "https://facebook.com/",
+        icons: "facebook-f",
+    },
+    {
+        title: "YouTube",
+        path: "https://youtube.com/",
+        icons: "youtube",
+    },
+    {
+        title: "Twitter",
+        path: "https://twitter.com/",
+        icons: "twitter",
+    },
+    {
+        title: "Instagram",
+        path: "https://instagram.com/",
+        icons: "instagram",
+    },
+];
+export default social;
