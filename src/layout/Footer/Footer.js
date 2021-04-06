b7f14e85bf226d8ace1c29ed825d346dee2fc579
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-04-06 20:05:34 +0200

    Update 6.04.2021 20:05
    Update Resposive Footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -5,22 +5,35 @@ import socials from "./social";
 export default function Footer() {
   return (
     <footer className="w-full bg-white dark:bg-gray-800 dark:text-white anime bottom-0 ">
-      <div className="grid grid-cols-2 grid-rows-2 p-4 mt-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:flex lg:justify-around grid-rows-2 md:grid-rows-none p-4 mt-4">
         <div className="place">
           <h1 className="pb-2">About Us</h1>
-          <p>Lorem </p>
+          <div className="lg:pl-5">
+            <p>O nas</p>
+            <p>Praca</p>
+            <p>Kariera</p>
+            <p>Reklam</p>
+          </div>
         </div>
         <div className="place">
           <h1 className="pb-2">Services</h1>
-          <p>Lorem, ipsum dolor.</p>
+          <div className="lg:pl-5">
+            <p>Metody płatności</p>
+            <p>Program partnerski</p>
+            <p>Program lojalnościowy</p>
+          </div>
         </div>
 
         <div className="place">
           <h1 className="pb-2">Contact</h1>
-          <a>Napisz do nas!</a>
+          <div className="lg:pl-5">
+            <p>Napisz do nas</p>
+            <p>Biuro Obsługi Klienta</p>
+            <p>Dane kontaktowe</p>
+          </div>
         </div>
         <div className="place">
-          <h1 className="pb-2">Social</h1>
+          <h1 className="pb-2 ">Social</h1>
           <div>
             {socials.map((social, index) => {
               return (
@@ -31,15 +44,20 @@ export default function Footer() {
                   target="_blank"
                 >
                   <i
-                    className={`fab fa-${social.icons} py-1 pr-2 grid grid-cols-4 text-center`}
+                    className={`fab fa-${social.icons} py-1 pr-2 grid grid-cols-4 lg:grid-cols-2 lg:text-center text-center md:text-left`}
                   >
-                    <p className="grid col-auto">{social.title}</p>
+                    <p className="grid col-auto lg:text-left">{social.title}</p>
                   </i>
                 </a>
               );
             })}
           </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center h-full pb-5 md:p-5">
+        <p className="text-sm text-center">
+          Copyright © 2021 TK. Wszelkie prawa zastrzeżone.
+        </p>
       </div>
     </footer>
   );
