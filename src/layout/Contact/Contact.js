bbe0e83a497dbf5884ac242d9928a4ac6cd21138
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-04-10 15:40:25 +0200

    Update 10.04.2021 15:39
    Update Footer and Contact.js on responsive

--- src/layout/Contact/Contact.js
+++ src/layout/Contact/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="flex overflow-hidden">
+    <div className="flex overflow-hidden min-h-screen">
       <h1 className="text-4xl m-auto">Napisz do Mnie</h1>
     </div>
   );
--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -5,7 +5,15 @@ import socials from "./social";
 export default function Footer() {
   return (
     <footer className="w-full bg-white dark:bg-gray-800 dark:text-white anime bottom-0 ">
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:flex lg:justify-around grid-rows-2 md:grid-rows-none p-4 mt-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-x-2 sm:gap-x-20 md:gap-x-0 grid-rows-2 md:grid-rows-none lg:flex lg:justify-around p-4 mt-4">
+        <div className="place">
+          <h1 className="pb-2">Services</h1>
+          <div className="lg:pl-5">
+            <p>Metody płatności</p>
+            <p>Program partnerski</p>
+            <p>Program lojalnościowy</p>
+          </div>
+        </div>
         <div className="place">
           <h1 className="pb-2">About Us</h1>
           <div className="lg:pl-5">
@@ -16,15 +24,6 @@ export default function Footer() {
           </div>
         </div>
         <div className="place">
-          <h1 className="pb-2">Services</h1>
-          <div className="lg:pl-5">
-            <p>Metody płatności</p>
-            <p>Program partnerski</p>
-            <p>Program lojalnościowy</p>
-          </div>
-        </div>
-
-        <div className="place">
           <h1 className="pb-2">Contact</h1>
           <div className="lg:pl-5">
             <p>Napisz do nas</p>
@@ -32,7 +31,7 @@ export default function Footer() {
             <p>Dane kontaktowe</p>
           </div>
         </div>
-        <div className="place">
+        <div className="place md:ml-5">
           <h1 className="pb-2 ">Social</h1>
           <div>
             {socials.map((social, index) => {
@@ -42,12 +41,13 @@ export default function Footer() {
                   key={index}
                   rel="noreferrer"
                   target="_blank"
+                  className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-2"
                 >
                   <i
-                    className={`fab fa-${social.icons} py-1 pr-2 grid grid-cols-6 lg:grid-cols-2 lg:text-center text-left`}
+                    className={`fab fa-${social.icons} py-1 pr-2 lg:text-center`}
                   >
-                    <p className="grid col-auto lg:text-left">{social.title}</p>
                   </i>
+                    <p className="">{social.title}</p>
                 </a>
               );
             })}
