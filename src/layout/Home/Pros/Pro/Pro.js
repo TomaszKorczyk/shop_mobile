f8371042c74ecbab30c0627f6ae29db8d56b0386
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-04-23 20:09:16 +0200

    Update 23.04.2021 20:09
    Update to view product list

--- src/layout/Home/Pros/Pro/Pro.js
+++ src/layout/Home/Pros/Pro/Pro.js
@@ -3,30 +3,33 @@ import "./style.css";
 
 export const Pro = ({ product }) => {
   return (
-    <div className="m-1 border border-black rounded-md overflow-hidden">
-      <div className="relative flex justify-center imgs">
-        <img
-          className="absolute h-full top-0 object-contain shadow-lg rounded-lg bg-white"
-          src={product.ProductImg}
-          alt={product.ProductName}
-        />
+    <div className="mb-2 border border-gray-500 bg-white dark:bg-gray-800  overflow-hidden anime">
+      <div>
+        <div className="relative flex justify-center imgs mt-1">
+          <img
+            className="absolute h-full top-0 object-contain shadow-lg rounded-lg"
+            src={product.ProductImg}
+            alt={product.ProductName}
+          />
+        </div>
       </div>
-      <section className="p-3">
-        <h4 className="w-full text-center font-semibold tracking-wider text-gray-800">
-          " {product.ProductName} "
+      <section className="px-2 pt-1">
+        <h4 className="w-full text-center capitalize text-lg text-gray-800 dark:text-white">
+          {product.ProductName}
         </h4>
-        <h5 className="w-full text-right tracking-wide text-gray-700">
+        <h5 className="w-full pr-2 text-right tracking-wide text-gray-700 opacity-70 dark:text-white">
           {product.ProductPrice}.00 zł
         </h5>
-
-        <p>
+        <p className="text-justify text-sm dark:text-gray-200 tracking-wider">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
           expedita.
         </p>
       </section>
-      <button className="flex w-full text-center justify-center items-center m-3 bg-red-400 rounded-xl cursor-pointer">
-        Add to cart
-      </button>
+      <div className="flex justify-center py-1">
+        <button className="bg-blue-300 dark:hover:text-black dark:bg-yellow-300 rounded-xl cursor-pointer px-4 py-0.5 my-1 focus:outline-none anime">
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 };
--- src/layout/Home/Pros/Pros.js
+++ src/layout/Home/Pros/Pros.js
@@ -7,16 +7,16 @@ export const Pros = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <main>
+    <main className="mb-5">
       {products.length !== 0 && (
         <h1 className="flex justify-center font-bold tracking-wider uppercase py-4 text-2xl">
           Products
         </h1>
       )}
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {products.length === 0 && <Loading />}
         {products.map((product) => (
-          <div className="grid" key={product.ProductID}>
+          <div className="grid mx-1" key={product.ProductID}>
             <Pro product={product} />
           </div>
         ))}
