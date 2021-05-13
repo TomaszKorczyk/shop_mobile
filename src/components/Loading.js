c22310b44bca2aae6b49a8a6056223f8bdb2098d
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-05-13 21:20:22 +0200

    Update 13.05. 21:20
    Style init form

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex w-full min-h-screen">
-      <p className="m-auto">
+    <div className="w-full init">
+      <p className="flex h-full">
         <i className="fas fa-circle-notch fa-spin text-5xl text-blue-800"></i>
       </p>
     </div>
--- src/layout/Contact/Contact.js
+++ src/layout/Contact/Contact.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="flex w-full h-full">
-      <h1 className="text-4xl m-auto">Napisz do Mnie</h1>
+    <div className="w-full init">
+      <div className="flex h-full">
+        <h1 className="text-4xl m-auto">Napisz do Mnie</h1>
+      </div>
     </div>
   );
 }
